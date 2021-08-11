facb39b2289d0c2bea3254aa6a556e658f854174
adrianmurage <adrianmurage21@gmail.com>
2021-08-11 11:21:43 +0300

    recursive factorial solution

--- recursion/factorial/Program.cs
+++ recursion/factorial/Program.cs
@@ -1,5 +1,6 @@
 ﻿/* 
-
+Write a recursive implementation of the factorial function. Recall that n! = 1 × 2 × ... × n,
+with the special case that 0! = 1.
 */
 using System;
 
@@ -9,7 +10,19 @@ namespace factorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter positive integer: ");
+            int positiveInt = int.Parse(Console.ReadLine());
+
+            int factorial = Factorial(positiveInt);
+            Console.WriteLine($"Factorial of {positiveInt} is {factorial}");
+
+        }
+
+        static int Factorial (int num)
+        {
+            if (num == 0 || num == 1)
+                return 1;
+            return num * Factorial(num - 1);
         }
     }
 }
