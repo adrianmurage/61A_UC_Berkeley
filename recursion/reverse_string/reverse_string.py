9eed023a49751d550622e9d08c30ebce37daa53b
adrianmurage <adrianmurage21@gmail.com>
2021-08-11 14:12:18 +0300

    add reverse string soln

--- recursion/reverse_string/reverse_string.py
+++ recursion/reverse_string/reverse_string.py
@@ -1,3 +1,6 @@
+"""
+Write a recursive function that, given a string s, prints the characters of s in reverse order
+"""
 def main():
     reversed_string = reverse_string(input("Enter string:"))
     print(reversed_string)
@@ -6,5 +9,5 @@ def reverse_string(input_string):
     if (input_string == ""):
         return ""
     return reverse_string(input_string[1:]) + input_string[0]
-    
+
 main()
