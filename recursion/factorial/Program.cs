155e3f043ab3e2213b6ff2bc3001377e12f58927
adrianmurage <adrianmurage21@gmail.com>
2021-08-11 10:37:05 +0300

    add c# factorial file solution

---
+++ recursion/factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+
+*/
+using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
